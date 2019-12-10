26bd3aebc01b76b06186361ddb980f5b2f81ea4f
Yuusuke KOUNOIKE <yuusuke.kounoike@futurestandard.co.jp>
2019-12-10 20:03:53 +0900

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 def main():
     setup(
         name='scorer-cloudprocessing',
-        version='0.1.2',
+        version='0.1.3',
         description='A Software Development Kit for Scorer Cloud Processing',
         packages=['scorer'],
         install_requires=['pyzmq', 'numpy']
